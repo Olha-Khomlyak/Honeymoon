60ab5fa96231b2c7dd20be56f3f5a6fc4f3b58fc
olha-khomlyak <olga.khomlyak@gmail.com>
2024-01-29 12:58:39 +0300

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by A. Faruk Acar on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
